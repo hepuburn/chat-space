7f59ddf6d66def789bffc0993f0cfc6cc1ffa12b
hepuburn <57085978+hepuburn@users.noreply.github.com>
2019-11-29 16:16:34 +0900

    非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,26 @@
 $(function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message">
+    <div class="upper-message">
+    <div class="upper-message__user-name">
+    ${message.user_name}
+    </div>
+    <div class="upper-message__date">
+    ${message.date}
+    </div>
+    </div>
+    <div class="lower-message">
+    <p class="lower-message__content">
+    ${content}
+    ${img}
+    </p>
+    
+    </div>
+    </div>`
+    return html;
+  }
     $('#new_message').on('submit', function(e){
       e.preventDefault();
       var message = new FormData(this);
@@ -11,6 +33,19 @@ $(function(){
         dataType: 'json',
         processData: false,
         contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("form")[0].reset();
+      })
+      
+      .fail(function(data){
+        alert('エラーが発生したためメッセージは送信できませんでした。');
+      })
+      .always(function(data){
+        $('.form__submit').prop('disabled', false);
       })
     })
   })
